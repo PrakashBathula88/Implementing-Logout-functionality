56b19d10cd53972eae96093cfc8527e82855b3cf
bathulapraka <bathulaprakash406@gmail.com>
2024-03-29 22:42:51 +0530

    DummyItems

---
+++ App.js
@@ -0,0 +1,12 @@
+import Nav from "./Components/Nav/Nav";
+import Dummy from "./Components/Dummyitems/Dummy";
+function App() {
+  return (
+    <div >
+    <Nav/>
+    <Dummy/>
+    </div>
+  );
+}
+
+export default App;
---
+++ Components/Nav/Nav.js
@@ -0,0 +1,15 @@
+const Nav=()=>{
+  return(
+  <div>
+    <span>HOME</span>
+    <span>STORE</span>
+    <span>ABOUT</span>
+    <span>CART</span>
+
+    <h1>The Generics</h1>
+    <h3>Music</h3>
+  </div>
+  );
+};
+
+export default Nav;
