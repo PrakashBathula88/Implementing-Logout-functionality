b973b27553f3b19f83f5df676decac83e845d9bd
bathulapraka <bathulaprakash406@gmail.com>
2024-04-15 06:49:46 +0530

    Making Api calls on React

--- Components/Dummyitems/Dummy.js
+++ Components/Dummyitems/Dummy.js
@@ -1,85 +1,94 @@
-import React from "react";
-import "../Dummyitems/Phones.css"
+import React, { useEffect, useState } from "react";
+import "../Dummyitems/Phones.css";
 import { IoMdCart } from "react-icons/io";
 import { LiaRupeeSignSolid } from "react-icons/lia";
 import { useProductContext } from "../AddCart/CartProviders";
 const Dummy = () => {
-    const{ AddTocart}=useProductContext();
-  const productsArr = [
-    {
-      title: "Iphone 12",
+  const { AddTocart } = useProductContext();
+  const [Phones, setPhones] = useState([]);
+  useEffect(() => {
+    const FetchingProducts = async () => {
+      try {
+      
+        const productsArr = [
+          {
+            title: "Iphone 12",
 
-      price: "98,899",
+            price: "98,899",
 
-      imageUrl:
-        "https://rukminim2.flixcart.com/image/312/312/xif0q/mobile/k/l/l/-original-imagtc5fz9spysyk.jpeg?q=70",
-        
-    },
+            imageUrl:
+              "https://rukminim2.flixcart.com/image/312/312/xif0q/mobile/k/l/l/-original-imagtc5fz9spysyk.jpeg?q=70",
+          },
 
-    {
-      title: "One Plus 12",
+          {
+            title: "One Plus 12",
 
-      price: "69,999",
+            price: "69,999",
 
-      imageUrl:
-        "https://m.media-amazon.com/images/I/41pR0qlI0yL._SX300_SY300_QL70_FMwebp_.jpg",
-       
-    },
+            imageUrl:
+              "https://m.media-amazon.com/images/I/41pR0qlI0yL._SX300_SY300_QL70_FMwebp_.jpg",
+          },
 
-    {
-      title: "Realme ",
+          {
+            title: "Realme ",
 
-      price: "19,999",
+            price: "19,999",
 
-      imageUrl:
-        "https://m.media-amazon.com/images/I/41tSQSq1xJL._SX300_SY300_QL70_FMwebp_.jpg",
+            imageUrl:
+              "https://m.media-amazon.com/images/I/41tSQSq1xJL._SX300_SY300_QL70_FMwebp_.jpg",
+          },
 
-        
-    },
+          {
+            title: "Realme Nazro",
 
-    {
-      title: "Realme Nazro",
+            price: "17,999",
 
-      price: "17,999",
+            imageUrl:
+              "https://m.media-amazon.com/images/I/8195A49fZbL._AC_UY327_FMwebp_QL65_.jpg",
+          },
+          {
+            title: "Redmi ",
 
-      imageUrl:
-        "https://m.media-amazon.com/images/I/8195A49fZbL._AC_UY327_FMwebp_QL65_.jpg",
-        
+            price: "17,889",
 
-    },
-    {
-      title: "Redmi ",
+            imageUrl:
+              "https://m.media-amazon.com/images/I/813ZN8Pj-HL._AC_UY327_FMwebp_QL65_.jpg",
+          },
+        ];
+        setPhones(productsArr);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    FetchingProducts();
+  }, []);
+  const AddProductsToCart = (product) => {
+    AddTocart(product);
+  };
 
-      price: "17,889",
-
-      imageUrl:
-        "https://m.media-amazon.com/images/I/813ZN8Pj-HL._AC_UY327_FMwebp_QL65_.jpg",
-    
-    },
-  ];
-
-  const AddProductsToCart=(product)=>{
-   AddTocart(product)
-  }
-  
   return (
     <div>
       <div className="container">
-        {productsArr.map((product, index) => (
-          <div key={index}  className="All_products">
+        {Phones.map((product, index) => (
+          <div key={index} className="All_products">
             <span>
-            <img src={product.imageUrl} alt={product.title}></img>
+              <img src={product.imageUrl} alt={product.title}></img>
             </span>
             <span className="PRODUCT">
-            <h1>{product.title}</h1>
-           
-            <span className="price_logo"><LiaRupeeSignSolid />{product.price}</span>
-            
-            <button  className="Cart_logo" onClick={()=>AddProductsToCart(product)}>
-              <IoMdCart />
-              Cart
-            </button>
-           
+              <h1>{product.title}</h1>
+
+              <span className="price_logo">
+                <LiaRupeeSignSolid />
+                {product.price}
+              </span>
+
+              <button
+                className="Cart_logo"
+                onClick={() => AddProductsToCart(product)}
+              >
+                <IoMdCart />
+                Cart
+              </button>
             </span>
           </div>
         ))}
--- Components/Home/Home.js
+++ Components/Home/Home.js
@@ -1,65 +1,80 @@
 import React from "react";
 import { FaRegCirclePlay } from "react-icons/fa6";
-import  "../Home/Home.css"
+import "../Home/Home.css";
 const Albums = () => {
   return (
     <div>
       <div className="Heading_Generics">
         <span>
-        <h1>THE GENERICS</h1>
-        <h2>Get our Latest Album</h2>
-        
-        <FaRegCirclePlay  style={{display:"flex",justifyContent:"center",alignItems:"center",padding:"20px",width:"15%",height:"20%",marginLeft:"300px",color:"skyblue",marginTop:"-10px"}}/>
+          <h1>THE GENERICS</h1>
+          <h2>Get our Latest Album</h2>
+
+          <FaRegCirclePlay
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              padding: "20px",
+              width: "15%",
+              height: "20%",
+              marginLeft: "300px",
+              // color: "skyblue",
+              backgroundColor:"1px solid skyblue",
+              marginTop: "-10px",
+            }}
+          />
         </span>
       </div>
-      <div>
+
+      <div className="All_tables_generics">
         <h1>Tours</h1>
-        <div>
-          <div>
-            <span>JUL16</span>
-            <span>DETROIT, MI</span>
-            <span>DTE ENERGY MUSIC THEATRE</span>
-            <button>BUY TICKETS</button>
+
+        <div className="data_details">
+          <div className="Generic_Data">
+            <span className="Album_data">JUL16</span>
+            <span className="Album_min">DETROIT, MI</span>
+            <span className="Album_Ener">DTE ENERGY MUSIC THEATRE</span>
+            <button className="Album_buttton">BUY TICKETS</button>
           </div>
-          <div>
-            <span>JUL19</span>
-            <span>TORONTO,ON</span>
-            <span>BUDWEISER STAGE</span>
-            <button>BUY TICKETS</button>
+          <div className="Generic_Data">
+            <span className="Album_data">JUL19</span>
+            <span className="Album_min">TORONTO,ON</span>
+            <span className="Album_Ener">BUDWEISER STAGE</span>
+            <button className="Album_buttton">BUY TICKETS</button>
           </div>
-          <div>
-            <span>JUL 22</span>
-            <span>BRISTOW, VA</span>
-            <span>JIGGY LUBE LIVE</span>
-            <button>BUY TICKETS</button>
+          <div className="Generic_Data">
+            <span className="Album_data">JUL 22</span>
+            <span className="Album_min">BRISTOW, VA</span>
+            <span className="Album_Ener">JIGGY LUBE LIVE</span>
+            <button className="Album_buttton">BUY TICKETS</button>
           </div>
-          <div>
-            <span>JUL 29</span>
-            <span>PHOENIX, AZ</span>
-            <span>AK-CHIN PAVILION</span>
-            <button>BUY TICKETS</button>
+          <div className="Generic_Data">
+            <span className="Album_data">JUL 29</span>
+            <span className="Album_min">PHOENIX, AZ</span>
+            <span className="Album_Ener">AK-CHIN PAVILION</span>
+            <button className="Album_buttton">BUY TICKETS</button>
           </div>
-          <div>
-            <span>AUG 2</span>
-            <span>LAS VEGAS, NV</span>
-            <span>T-MOBILE ARENA</span>
-            <button>BUY TICKETS</button>
+          <div className="Generic_Data">
+            <span className="Album_data">AUG 2</span>
+            <span className="Album_min">LAS VEGAS, NV</span>
+            <span className="Album_Ener">T-MOBILE ARENA</span>
+            <button className="Album_buttton">BUY TICKETS</button>
           </div>
-          <div>
-            <span>AUG 7</span>
-            <span>CONCORD, CA</span>
-            <span>CONCORD PAVILION</span>
-            <button>BUY TICKETS</button>
+          <div className="Generic_Data">
+            <span className="Album_data">AUG 7</span>
+            <span className="Album_min">CONCORD, CA</span>
+            <span className="Album_Ener">CONCORD PAVILION</span>
+            <button className="Album_buttton">BUY TICKETS</button>
           </div>
         </div>
       </div>
+
       <div>
-          <div>
-            <h1>The Generics</h1>
-          </div>
+        <div className="The_Generic">
+          <h1>The Generics</h1>
+        </div>
       </div>
     </div>
-  
   );
 };
 export default Albums;
